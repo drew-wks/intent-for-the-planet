63276a99eee8300b271892164894a54dd45c94d9
Drew Wilkins <20048187+drew-wks@users.noreply.github.com>
2024-01-08 08:24:26 -0500

    fs

--- utils.py
+++ utils.py
@@ -66,7 +66,7 @@ def query(question, response_file):
 
     messages =  [  
         {'role':'system', 
-        'content': "You are a helpful assistant. Try to answer the users question based on the info in the json provided. YOu can add information beyond the json as long as you let user know the inforamtion did not come from the json"},    
+        'content': "You are a helpful assistant. Try to answer the users question based on the info in the INTENTs provided. YOu can add information beyond the INTENTs as long as you let user know the inforamtion did not come from the json"},    
         {'role':'user', 
         'content': f"""{question}```{data}```"""}  
     ] 
