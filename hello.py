03c42ff1fc6669b57ae03f0fbbb480590a745e8d
Drew Wilkins <20048187+drew-wks@users.noreply.github.com>
2024-01-08 13:44:52 -0500

    fs

--- hello.py
+++ hello.py
@@ -36,6 +36,18 @@ st.markdown("""
             font-family: 'Lexend', sans-serif;
             font-weight: 500;
         }
+        
+        .h3-style {
+            font-size: 1.17em; /* Typical size of h3 */
+            font-weight: bold; /* h3 is usually bold */
+            /* Add other h3 styles if needed */
+        }
+
+        .h4-style {
+            font-size: 1em; /* Typical size of h4 */
+            font-weight: bold; /* h4 is usually bold */
+            /* Add other h4 styles if needed */
+        }
 
         /* Other styles */
         [data-testid="collapsedControl"] { display: none; }
@@ -52,7 +64,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 
-st.markdown('<h3 class="logotype">INTENT<em>®</em><span class="header" style="color: black;"> for the Planet</span></h3>', unsafe_allow_html=True)
+st.markdown('<span class="logotype">INTENT<em>®</em></span><span class="header h4-style" style="color: black;"> for the Planet</span>', unsafe_allow_html=True)
 
 st.markdown("""
     <style>
