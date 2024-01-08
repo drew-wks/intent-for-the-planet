3e454b43bce60e11810318ad2e9a9d35c798864c
Drew Wilkins <20048187+drew-wks@users.noreply.github.com>
2024-01-07 19:47:19 -0500

    fs

--- hello.py
+++ hello.py
@@ -56,7 +56,7 @@ with tab1:
             utils.save_responses(user_responses)
             st.success("Thank you for your responses!")
             for key, value in user_responses.items():
-                st.markdown(f"{key}:\n {value}")
+                st.markdown(f"{key}:\n\n  {value}")
 
 
 with tab2:
