8b32bac95b0301b6fae45d55341511cc16e79fdc
Drew Wilkins <20048187+drew-wks@users.noreply.github.com>
2024-01-08 16:12:12 -0500

    fs

--- hello.py
+++ hello.py
@@ -143,7 +143,7 @@ with tab3:
     st.markdown('<div style="margin-top: 40px;"></div>', unsafe_allow_html=True)
     st.markdown('<span class="sub-header markdown-text-container">Ask a question of the collection of INTENTs:</span>', unsafe_allow_html=True)
     question = st.text_input("", placeholder="Type your question here")
-    synthetic = st.checkbox('Include synthetic data')
+    synthetic = st.toggle('Include synthetic data')
     response_file = "responses/responses_real.json" if not synthetic else "responses/responses_all.json"
 
     if st.button("Submit", type="primary"):
